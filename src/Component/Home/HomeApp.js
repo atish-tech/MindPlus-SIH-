37684376a3721000d25035dc0fc30f90be6072db
Aatish <katish332@gmail.com>
2023-09-15 22:21:07 +0530

    change loader

--- src/Component/Home/HomeApp.js
+++ src/Component/Home/HomeApp.js
@@ -3,15 +3,16 @@ import "./Home.css"
 // import Footer from "../Footer/Footer";
 import { Clouser } from './Closer/Clouser';
 import HomeContent from './Content/HomeContent';
+// import { Navigation } from '@mui/icons-material';
 
 
 const HomeApp = () => {
   return (
     <>
+
         <div className='home-component'>
         <Clouser />
         <HomeContent />
-        
           {/* <Footer  /> */}
         </div>
     </>
--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -5,6 +5,7 @@ import axios from "axios";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import "./Login.css"
+import { Circles } from 'react-loader-spinner';
 
 const Login = () => {
     const notify = (p) => toast(p);
@@ -19,7 +20,7 @@ const Login = () => {
     // login
     const loginHandler = async () => {
         // navigater("home");
-        if(data.email.length < 1 || data.password < 1){
+        if (data.email.length === "" || data.password === "") {
             notify("Input field is empty");
             return;
         }
@@ -44,14 +45,14 @@ const Login = () => {
         }
         catch (e) {
             // console.log(e);
-            notify("error occure")
+            notify("error occe")
         }
         setLoader(false)
     }
     // register 
     const registerHandler = async () => {
 
-        if(data.name.length < 1 || data.email.length < 1 || data.password < 1){
+        if (data.name.length < 1 || data.email.length < 1 || data.password < 1) {
             notify("Input field is empty");
             return;
         }
@@ -85,24 +86,30 @@ const Login = () => {
             <div className='login-con-main'>
                 { // Sign Up
                     seeLogin && (
-                        <div style={{backgroundColor: "#0F2C59"}} className='login-container shadow-blue-gray-800 px-4'>
+                        <div style={{ backgroundColor: "#0F2C59" }} className='login-container shadow-blue-gray-800 px-4'>
                             <div className='login-image'>
                                 <img style={{ borderRadius: "30px" }} width={300} src='https://i.ibb.co/XV7VJ2s/Screenshot-2023-09-14-122032.png' />
                             </div>
-                            <div style={{backgroundColor: "#213555"}} className='login-input-container '>
+                            <div style={{ backgroundColor: "#213555" }} className='login-input-container '>
                                 <div className='login-input-item'>
                                     <div>
                                         <h1 className='text-blue-gray-500' >Create your Account</h1>
                                     </div>
-                                    <TextField style={{backgroundColor: "#053B50"}} className='' name='name' onChange={onChangeHandler} value={data.name} size='large' id="standard-basic" label="User Id" variant="outlined" />
-                                    <TextField style={{backgroundColor: "#053B50"}} className='' name='email' onChange={onChangeHandler} value={data.email} size='large' id="standard-basic" label="Email" variant="outlined" />
-                                    <TextField style={{backgroundColor: "#053B50"}} className='' name='password' onChange={onChangeHandler} value={data.password} size='large' id="standard-basic" type='password' label="Password" variant="outlined" />
+                                    <TextField className='' name='name' onChange={onChangeHandler} value={data.name} size='large' id="standard-basic" label="User Id" variant="outlined" />
+                                    <TextField className='' name='email' onChange={onChangeHandler} value={data.email} size='large' id="standard-basic" label="Email" variant="outlined" />
+                                    <TextField className='' name='password' onChange={onChangeHandler} value={data.password} size='large' id="standard-basic" type='password' label="Password" variant="outlined" />
                                     <Button onClick={registerHandler} style={{ color: "green" }} size='large' variant="outlined">Sign Up</Button>
                                     {
                                         loader &&
-                                        <Box sx={{ display: 'flex' }}>
-                                            <CircularProgress />
-                                        </Box>
+                                        <Circles
+                                            height="80"
+                                            width="80"
+                                            color="#4fa94d"
+                                            ariaLabel="circles-loading"
+                                            wrapperStyle={{}}
+                                            wrapperClass=""
+                                            visible={true}
+                                        />
                                     }
                                     <h3 style={{ color: "black" }} >Already have a <span onClick={() => setSeeLogin(!seeLogin)} className='register-btn'>Account</span> </h3>
                                     <ToastContainer />
@@ -115,11 +122,11 @@ const Login = () => {
 
                 { // Login
                     !seeLogin && (
-                        <div style={{backgroundColor: "#0F2C59"}} className='login-container shadow-blue-gray-800 px-4'>
+                        <div style={{ backgroundColor: "#0F2C59" }} className='login-container shadow-blue-gray-800 px-4'>
                             <div className='login-image'>
                                 <img style={{ borderRadius: "30px" }} width={300} src='https://i.ibb.co/XV7VJ2s/Screenshot-2023-09-14-122032.png' />
                             </div>
-                            <div style={{backgroundColor: "#213555"}} className='login-input-container '>
+                            <div style={{ backgroundColor: "#213555" }} className='login-input-container '>
                                 <div className='login-input-item'>
                                     <div>
                                         <h1 className='text-blue-gray-500'>Login To your Account</h1>
@@ -129,11 +136,16 @@ const Login = () => {
                                     <Button onClick={loginHandler} style={{ color: "green" }} size='large' variant="outlined">Login</Button>
                                     {
                                         loader &&
-                                        <Box sx={{ display: 'flex' }}> 
+                                        <Circles
+                                            height="80"
+                                            width="80"
+                                            color="#4fa94d"
+                                            ariaLabel="circles-loading"
+                                            wrapperStyle={{}}
+                                            wrapperClass=""
+                                            visible={true}
+                                        />
 
-                                        < CircularProgress color="inherit" />
-
-                                    </Box>
                                     }
                                     <h3 style={{ color: "black" }} >First Time <span onClick={() => setSeeLogin(!seeLogin)} className='register-btn'>User</span></h3>
                                     <ToastContainer />
